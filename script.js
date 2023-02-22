0ce9fd18939f113e2acc7746bb0915c6c34ea464
FiveXC <juanpablopereiracavalcante@gmail.com>
2023-02-22 11:41:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/*||*/
+let oláEuSouJuan = document.querySelector(".oláEuSouJuan")
+let acordeon = document.querySelectorAll(".acordeon")
+
+acordeon.forEach(function(item){
+    
+item.addEventListener("click", ()=>{
+
+
+
+let moverExiste = document.querySelector(".mover2")
+if(moverExiste){
+    moverExiste.classList.remove("mover2")
+}
+
+ if(item !== moverExiste){
+ item.classList.add("mover2")
+}
+
+})
+})
+
+window.onload = () => {
+    oláEuSouJuan.classList.add("mover")
+ }
